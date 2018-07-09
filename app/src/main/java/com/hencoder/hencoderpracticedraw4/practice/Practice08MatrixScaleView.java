f3cc5dbdbbb6e3df9a510d844a807a58d593e886
wsf <way1397@163.com>
2018-07-09 11:13:13 +0800

    1.练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice08MatrixScaleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice08MatrixScaleView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.support.annotation.Nullable;
@@ -17,6 +18,7 @@ public class Practice08MatrixScaleView extends View {
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    Matrix mMatrix = new Matrix();
 
     public Practice08MatrixScaleView(Context context) {
         super(context);
@@ -37,8 +39,21 @@ public class Practice08MatrixScaleView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
 
+        canvas.save();
+        mMatrix.reset();
+        mMatrix.postScale(0.5f, 0.5f, point1.x + width / 2, point1.y + height / 2);
+        canvas.concat(mMatrix);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+        mMatrix.reset();
+        mMatrix.postScale(0.5f, 1.3f, point2.x + width / 2, point2.y + height / 2);
+        canvas.concat(mMatrix);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
